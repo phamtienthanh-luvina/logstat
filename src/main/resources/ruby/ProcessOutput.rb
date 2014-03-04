98caf8a56a71faf5abfdecc3e9be7d092fd607eb
Do Thi Huong <dothihuong.luvina@gmail.com>
2014-03-04 16:52:41 +0700

    process output

--- src/main/resources/ruby/ProcessOutput.rb
+++ src/main/resources/ruby/ProcessOutput.rb
@@ -1,155 +1,154 @@
 require 'ruby/libs/Common.rb'
-
 include CommonUtils
-CommonUtils.require_gem('rubygems')
-#CommonUtils.require_gem('bson_ext')
-#CommonUtils.require_gem('mongo')
-CommonUtils.require_gem('net/http')
-#CommonUtils.require_gem('active_support')
-CommonUtils.require_gem('json')
-#include Mongo
 
 class ProcessOutput
-  ##
-  # output: process output
-  # @param data: data receive after process filter
-  # @param output_conf: output configuration from job
-  # @param map_default_output: default information for output
-  ##
-  def output(data, output_conf, map_default_output)
-    if(!data.nil?)
-      if(!data["list_logs"].nil? && !data["list_logs"].empty?)
-        # Get output type
-        outputType = "file"
-        if output_conf['type'] != nil && output_conf['type'] != ''
-          outputType = output_conf['type']
-        end
+	##
+	 # output: process output
+	 # @param data: data receive after process filter
+	 # @param output_conf: output configuration from job
+	 # @param map_default_output: default information for output
+	##
+	def output(data, output_conf, map_default_output)
+		if(!data.nil?)
+			if(!data["list_logs"].nil? && !data["list_logs"].empty?)
+				# Get output type
+				outputType = "file"
+				if output_conf['type'] != nil && output_conf['type'] != ''
+					outputType = output_conf['type']
+				end
+				# Check outputType to call corresponding process
+				if outputType == "file" # Out to file/stdout
+					configFile = output_conf['config']
+					if configFile['path'] != nil && configFile['path'] != '' && configFile['path'] != "stdout"
+						self.writeToFile(data['list_logs'], configFile['path'])
+					else
+		        data['list_logs'].each do |eLog|
+		          puts "#{eLog}"
+		        end
+					end
+				elsif outputType == "mongoDB" # Put on MongoDB
+					# Call method process insert to mongo db
+					self.insertToMongo(data['list_logs'], output_conf['config'], map_default_output)
+				elsif outputType == "http" # Out to servlet
+					# Call method process send data to servlet
+					self.sendToServlet(data['list_logs'], output_conf['config'], map_default_output)
+				elsif outputType == "job" # Out to output job format
+					# Call method process put out format of job's output
+					self.putOutJobFormat(data)
+				end
+			else
+		        puts "[Logstat]: No data to output !"  
+			end
+	    else
+	      puts "[Logstat]: No data to output !"
+	    end
+	end
 
-        # Check outputType to call corresponding process
-        if outputType == "file" # Out to file/stdout
-          configFile = output_conf['config']
-          if configFile['path'] != nil && configFile['path'] != '' && configFile['path'] != "stdout"
-            self.writeToFile(data['list_logs'], configFile['path'])
-          else
-            data['list_logs'].each do |eLog|
-              puts "#{eLog}"
-            end
-          end
-        elsif outputType == "mongoDB" # Put on MongoDB
-          # Call method process insert to mongo db
+	##
+	 # writeToFile: write data to file
+	 # @param input: output data to write
+	 # @param destination: file path
+	##
+	def writeToFile(input, destination)
+		begin
+			File.open(destination, 'w') do |file|
+				input.each do |line|
+					file.write(line)
+				end	
+			end
+		rescue Exception => e
+			puts "Can not create/open file with path config !!!"
+		end
+	end
+	
+	##
+	 # insertToMongo: insert data to mongo db
+	 # @param data: output data to insert
+	 # @param mongoConf: configuration from job
+	##
+	def insertToMongo(data, mongoConf, mapDefaultOutput)
+		CommonUtils.require_gem('bson')
+		CommonUtils.require_gem('mongo')
+		CommonUtils.require_gem('bson_ext')
+		include Mongo
 
-          self.insertToMongo(data['list_logs'], output_conf['config'], map_default_output)
-        elsif outputType == "http" # Out to servlet
-          # Call method process send data to servlet
-          self.sendToServlet(data['list_logs'], output_conf['config'], map_default_output)
-        elsif outputType == "job" # Out to output job format
-          # Call method process put out format of job's output
-          self.putOutJobFormat(data)
-        end
-      else
-        puts "[Logstat]: No data to output !"  
-      end
-    else
-      puts "[Logstat]: No data to output !"
-    end
-  end
+		if mongoConf != {}
+			# Host
+			host = mapDefaultOutput['host']
+			if mongoConf['host'] != nil && mongoConf['host'] != ''
+				host = mongoConf['host']
+			end
+			# Port
+			port = mapDefaultOutput['port']
+			if mongoConf['port'] != nil && mongoConf['port'] != ''
+				port = mongoConf['port']
+			end
+			# Database name
+			dbName = mapDefaultOutput['dbName']
+			if mongoConf['dbName'] != nil && mongoConf['dbName'] != ''
+				dbName = mongoConf['dbName']
+			end
+			# Table insert
+			tblInsert = mapDefaultOutput['tblInsert']
+			if mongoConf['tblName'] != nil && mongoConf['tblName'] != ''
+				tblInsert = mongoConf['tblName']
+			end
+			# User, Password
+			user = mongoConf['user']
+			pass = mongoConf['pass']
 
-  ##
-  # writeToFile: write data to file
-  # @param input: output data to write
-  # @param destination: file path
-  ##
-  def writeToFile(input, destination)
-    begin
-      File.open(destination, 'w') do |file|
-        input.each do |line|
-          file.puts(line)
-        end
-      end
-    rescue Exception => e
-      puts "Can not create/open file with path config !!!"
-    end
-  end
+			# Process write data to mongodb
+			begin
+				# Get db connections
+				db = MongoClient.new(host, port).db(dbName)
+				db.authenticate(user, pass)
+				tbl_log = db.collection(tblInsert)
+				tbl_log.insert(data)
+				puts "SUCCESS: Write data to MongoDB (table: #{tblInsert})!!!"
+			rescue Exception => ex
+				puts "ERROR: #{ex}"
+			end
+		else
+			puts "Must be config information (host, port, dbName, user, pass) to put data to MongoDB !!!"
+		end
+	end
 
-  ##
-  # insertToMongo: insert data to mongo db
-  # @param data: output data to insert
-  # @param mongoConf: configuration from job
-  ##
-  def insertToMongo(data, mongoConf, mapDefaultOutput)
-    if mongoConf != {}
-      # Host
-      host = mapDefaultOutput['host']
-      if mongoConf['host'] != nil && mongoConf['host'] != ''
-        host = mongoConf['host']
-      end
-      # Port
-      port = mapDefaultOutput['port']
-      if mongoConf['port'] != nil && mongoConf['port'] != ''
-        port = mongoConf['port']
-      end
-      # Database name
-      dbName = mapDefaultOutput['dbName']
-      if mongoConf['dbName'] != nil && mongoConf['dbName'] != ''
-        dbName = mongoConf['dbName']
-      end
-      # Table insert
-      tblInsert = mapDefaultOutput['tblInsert']
-      if mongoConf['tblName'] != nil && mongoConf['tblName'] != ''
-        tblInsert = mongoConf['tblName']
-      end
-      # User, Password
-      user = mongoConf['user']
-      pass = mongoConf['pass']
+	##
+	 # sendToServlet: send data to servlet
+	 # @param data: output data to send
+	 # @param servletConf: configuration from job
+	##
+	def sendToServlet(data, servletConf, mapDefaultOutput)
+		CommonUtils.require_gem('net/http')
+		CommonUtils.require_gem('active_support')
+		CommonUtils.require_gem('json')
 
-      # Process write data to mongodb
-      begin
-        # Get db connections
-        db = MongoClient.new(host, port).db(dbName)
-        db.authenticate(user, pass)
-        tbl_log = db.collection(tblInsert)
-        tbl_log.insert(data)
-        puts "SUCCESS: Write data to MongoDB (table: #{tblInsert})!!!"
-      rescue Exception => ex
-        puts "ERROR: #{ex}"
-      end
-    else
-      puts "Must be config information (host, port, dbName, user, pass) to put data to MongoDB !!!"
-    end
-  end
+		pathConf = mapDefaultOutput['pathConf']
+		if servletConf['path'] != nil && servletConf['path'] != ''
+			pathConf = servletConf['path']
+			if !pathConf.include? "http"
+				pathConf = "http://#{pathConf}"
+			end
+		end
+		uri = URI.parse(pathConf)
+		@host = uri.host
+		@port = uri.port
+		@path = uri.path
 
-  ##
-  # sendToServlet: send data to servlet
-  # @param data: output data to send
-  # @param servletConf: configuration from job
-  ##
-  def sendToServlet(data, servletConf, mapDefaultOutput)
-    pathConf = mapDefaultOutput['pathConf']
-    if servletConf['path'] != nil && servletConf['path'] != ''
-      pathConf = servletConf['path']
-      if !pathConf.include? "http"
-        pathConf = "http://#{pathConf}"
-      end
-    end
-    uri = URI.parse(pathConf)
-    @host = uri.host
-    @port = uri.port
-    @path = uri.path
+		@body = ActiveSupport::JSON.encode(data)
+		
+		request = Net::HTTP::Post.new(@path, initheader = {'Content-Type' =>'application/json'})
+		request.body = @body
+		response = Net::HTTP.new(@host, @port).start {|http| http.request(request) }
+	end
 
-    @body = ActiveSupport::JSON.encode(data)
-
-    request = Net::HTTP::Post.new(@path, initheader = {'Content-Type' =>'application/json'})
-    request.body = @body
-    response = Net::HTTP.new(@host, @port).start {|http| http.request(request) }
-  end
-
-  ##
-  # putOutJobFormat:
-  # @param data: output data to send
-  # @param jobConf: configuration from job
-  # @return data
-  ##
-  def putOutJobFormat(data)
-    return data
-  end
+	##
+	 # putOutJobFormat: 
+	 # @param data: output data to send
+	 # @param jobConf: configuration from job
+	 # @return data
+	##
+	def putOutJobFormat(data)
+		return data
+	end
 end
